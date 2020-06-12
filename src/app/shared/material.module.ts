ad4f01fce7eb133f0c2bbc0bc247192f41aa924a
iharishnaidu <iharishnaidu@gmail.com>
2020-06-11 22:43:23 -0400

    Configured Bootstrap and Material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,91 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatCardModule,
+  MatButtonToggle,
+  MatButtonToggleModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule
+} from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material';
+import { MatTableModule } from '@angular/material';
+import { MatPaginatorModule } from '@angular/material';
+import { MatSortModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
+import { MatOptionModule } from '@angular/material';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material';
+import { MatRadioModule, MatCheckboxModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material';
+import { MatProgressSpinnerModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material';
+import { MatNativeDateModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
+
+@NgModule({
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
+  ],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
+  ],
+  declarations: []
+})
+export class MaterialModule {}
